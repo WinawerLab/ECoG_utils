b6f26aa8ef384c4c0f7786447cf08badb6367992
irisgroen <iris.groen@nyu.edu>
2019-05-10 16:00:59 -0400

    added analysis functions

---
+++ functions/analysis/bairanalysis_getepochs_visualelecs.m
@@ -0,0 +1,62 @@
+function [out] = bairanalysis_getepochs_visualelecs(dataLoc, sub_label, ses_label)
+
+% LOAD PREPROCESSED DATA
+
+dataNm  = fullfile(dataLoc, sprintf('sub-%s', sub_label), sprintf('ses-%s', ses_label),  sprintf('sub-%s_ses-%s_epoched.mat', sub_label, ses_label));
+%a{k}    = load(dataNm);
+fprintf('[%s] Loading preprocessed data file %s... \n', mfilename, dataNm);
+load(dataNm);
+
+% Collect info on matched electrodes
+viselec_wang = []; visarea_wang = [];
+for wangname = {'wang2015_atlas', 'wang15_mplbl'}
+    if ~isempty(trials.viselec.(wangname{:}))
+        viselec_wang = [viselec_wang trials.viselec.(wangname{:}).elec_labels];
+        visarea_wang = [visarea_wang trials.viselec.(wangname{:}).area_labels];
+    end
+end
+viselec_benson = []; visarea_benson = [];
+if ~isempty(trials.viselec.benson14_varea)
+    viselec_benson = [viselec_benson trials.viselec.benson14_varea.elec_labels];
+    visarea_benson = [visarea_benson trials.viselec.benson14_varea.area_labels];
+end
+
+% Get visual elec indices
+[viselec] = unique([viselec_wang viselec_benson])';
+name = cell(size(viselec));
+wang = cell(size(viselec));
+benson = cell(size(viselec));
+elInx = []; 
+for ii = 1:length(viselec)
+    elInx(ii) = ecog_matchChannels(viselec{ii}, trials)';
+    name{ii} = viselec{ii};
+    wangInx = strmatch(viselec{ii}, viselec_wang, 'exact');
+    if ~isempty(wangInx)
+        wang{ii} = visarea_wang{wangInx};
+    %else
+    %    wang{ii} = ' ';
+    end
+    bensonInx = strmatch(viselec{ii}, viselec_benson, 'exact');
+    if ~isempty(bensonInx)
+        benson{ii} = visarea_benson{bensonInx};
+    %else
+    %    benson{ii} = ' ';
+    end
+end
+visareas = table(name, wang, benson);
+
+% Select visual electrodes
+trials.broadband = trials.broadband(elInx,:,:);
+trials.evoked = trials.evoked(elInx,:,:);
+trials.channels = trials.channels(elInx,:);
+blank_trials.broadband = blank_trials.broadband(elInx,:,:);
+blank_trials.evoked = blank_trials.evoked(elInx,:,:);
+blank_trials.channels = blank_trials.channels(elInx,:);
+
+out.sub = sub_label;
+out.ses = ses_label;
+out.trials = trials;
+out.blank_trials = blank_trials;
+out.visareas = visareas;
+
+end
