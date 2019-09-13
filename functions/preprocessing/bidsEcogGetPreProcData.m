9b937c2dcbc046404130ca009e6057521dc78dd8
irisgroen <iris.groen@nyu.edu>
2019-09-13 13:08:36 -0400

    continuing to build

--- functions/preprocessing/bidsEcogGetPreProcData.m
+++ functions/preprocessing/bidsEcogGetPreProcData.m
@@ -2,23 +2,25 @@
 function [ts, channels, events] = bidsEcogGetPreProcData(dataPath, subject, sessions, tasks, runnums)
 
 % get timeseries, channels, events, concatenated for subset of tasks
-
+% should get timeseries, events and channel info for a subset of
+% tasks and runnums(optional) for a given data folder.
+    
 % [data, channels, events] = bidsEcogGetPreProcData(dataPath, subject, [sessions], [tasks], [runnums])
 
-
-for j = 1 : length(ses_label{k})
-
-
-
+[sessions] = bidsSpecifySessions(dataPath, subject, sessions);
+        
+for ii = 1:length(sessions)
 
     % bidsSpecifyData
-
-    % should get timeseries, events and channel info for a subset of
-    % tasks and runnums(optional) for a given preprocessed folder.
-    % runs and tasks(?) should be concatenated?
-
-    % use? [data, info] = bidsGetPreprocData(dataPath, dataStr, tasks, runnums);
-
-
-
+    [session, tasks, runnum] = bidsSpecifyData(projectDir, subject, sessions{ii}, tasks);
+    
+    % get time series
+    [ts, info] = bidsGetPreprocData(dataPath, dataStr, tasks, runnum);
+    
+    % get channels and events
+    
+    % do some checks, e.g. does channel table match the size of the ts
+    
+    % concatenate tasks and runs - add task and run indices to events 
+    
 end
