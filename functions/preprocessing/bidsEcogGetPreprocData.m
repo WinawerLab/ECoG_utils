38108726c81775f42e767fc102cd03393797eebd
irisgroen <iris.groen@nyu.edu>
2020-02-14 13:40:12 -0500

    adding workaround when no runs are found for a given task

--- functions/preprocessing/bidsEcogGetPreprocData.m
+++ functions/preprocessing/bidsEcogGetPreprocData.m
@@ -29,86 +29,105 @@ secondsToAdd = 0;
 
 if isempty(runnums), getRunsPerSession = 1; else, getRunsPerSession = 0; end
 runCount = 0;
+
 for ii = 1:length(sessions)
 
-    % bidsSpecifyData
+    % check how many runs there are for each task
     if getRunsPerSession, runnums = []; end
     [session, tasks, runnums] = bidsSpecifyData(dataPath, subject, sessions{ii}, tasks, runnums);
      
     for jj = 1:length(tasks)
-        
+
         task = tasks{jj};
         
-        fprintf('[%s] Concatenating runs for session %s, task %s...\n',mfilename, session, task);
-        
-        for kk = 1:length(runnums{jj})
-            
-            runnum = runnums{jj}{kk};
-            [data, channels, events, ~, hdr] = bidsEcogReadFiles(dataPath, subject, session, task, runnum, description);
-            
-            runCount = runCount + 1;
-            
-            % Run various checks on events table:
-            
-            % Check if there are trial_names, if not, add them
-            if ~isfield(summary(events), 'trial_name')
-                fprintf('[%s] Events lack trial names - adding them now .\n',mfilename);
-                events = bair_addTrialNamesToEventsTable(events);
-            end
-            % Check if there are trial_names, if not, add them
-            if ~isfield(summary(events), 'stim_file')
-                fprintf('[%s] Events lack stim files - adding them now .\n',mfilename);
-                events.stim_file = repmat({'n/a'}, [height(events) 1]);
-            end
-            
-            
-            % Check if there are sample indices and ISIs:
-            if ~isfield(summary(events),'event_sample'); events.event_sample = round(events.onset*hdr.Fs);end
-            if ~isfield(summary(events), 'ISI');events.ISI = zeros(height(events),1);end
-            
-            % Add task, session and run indices to the events file
-            if ~isfield(summary(events), 'task_name'), events.task_name = repmat({task}, [height(events),1]); end
-            events.session_name = repmat({session}, [height(events),1]);
-            events.run_name = repmat({runnum}, [height(events),1]);
-                
-            % Concatenate data and events; update onsets 
-            if runCount == 1 
-                allEvents = events; 
-                allData = data; 
-            end
-            if runCount > 1
-                events.event_sample = events.event_sample + samplesToAdd;
-                events.onset = events.onset + secondsToAdd;
-                allEvents = [allEvents; events];
-                allData = cat(2,allData,data);
-            end
-            
-            % Use runlengths to update next run
-            runLengthInSamples = hdr.nSamples;
-            runLengthInSeconds = hdr.nSamples/hdr.Fs;
-            samplesToAdd = samplesToAdd + runLengthInSamples;
-            secondsToAdd = secondsToAdd + runLengthInSeconds;
-            fprintf('[%s] Length of run %s is %s seconds, cumulative length of data is %s seconds \n', mfilename, runnum, num2str(runLengthInSeconds), num2str(samplesToAdd/hdr.Fs));          
-        end
-    end
+        % check if we have any runs this task in this session; 
+        % if not, continue to next session
     
-    % When concatenating across sessions, check channel statuses
-    if ii == 1, previousChannels = channels; end
-    if ii > 1
-        if isfield(summary(channels), 'status')
-            if ~isequal(channels.status, previousChannels.status)  
-                 % This means one or more electrodes are bad in one session
-                 % but not another. For now, label the electrode as good
-                 % across all sessions:
-                 channels.status(~strcmp(channels.status, previousChannels.status)) = {'good'};
-                 % and assume bad channels/epochs will removed at later
-                 % stages.
-                 % TO DO: find some smarter way to deal with this problem
+        if isempty(runnums{jj})
+            
+            fprintf('[%s] No runs for task %s in session %s \n',mfilename, task, session);
+            continue
+            
+        else
+            fprintf('[%s] Concatenating runs for session %s, task %s...\n',mfilename, session, task);
+
+            for kk = 1:length(runnums{jj})
+
+                runnum = runnums{jj}{kk};
+
+                [data, channels, events, ~, hdr] = bidsEcogReadFiles(dataPath, subject, session, task, runnum, description);
+
+                runCount = runCount + 1;
+
+                % Run various checks on events table:
+
+                % Check if there are trial_names, if not, add them
+                if ~isfield(summary(events), 'trial_name')
+                    fprintf('[%s] Events lack trial names - adding them now .\n',mfilename);
+                    events = bair_addTrialNamesToEventsTable(events);
+                end
+                % Check if there are trial_names, if not, add them
+                if ~isfield(summary(events), 'stim_file')
+                    fprintf('[%s] Events lack stim files - adding them now .\n',mfilename);
+                    events.stim_file = repmat({'n/a'}, [height(events) 1]);
+                end
+
+
+                % Check if there are sample indices and ISIs:
+                if ~isfield(summary(events),'event_sample'); events.event_sample = round(events.onset*hdr.Fs);end
+                if ~isfield(summary(events), 'ISI');events.ISI = zeros(height(events),1);end
+
+                % Add task, session and run indices to the events file
+                if ~isfield(summary(events), 'task_name'), events.task_name = repmat({task}, [height(events),1]); end
+                events.session_name = repmat({session}, [height(events),1]);
+                events.run_name = repmat({runnum}, [height(events),1]);
+
+                % Concatenate data and events; update onsets 
+                if runCount == 1 
+                    allEvents = events; 
+                    allData = data; 
+                end
+                if runCount > 1
+                    events.event_sample = events.event_sample + samplesToAdd;
+                    events.onset = events.onset + secondsToAdd;
+                    allEvents = [allEvents; events];                    
+                    allData = cat(2,allData,data);
+                end
+
+                % Use runlengths to update next run
+                runLengthInSamples = hdr.nSamples;
+                runLengthInSeconds = hdr.nSamples/hdr.Fs;
+                samplesToAdd = samplesToAdd + runLengthInSamples;
+                secondsToAdd = secondsToAdd + runLengthInSeconds;
+                fprintf('[%s] Length of run %s is %s seconds, cumulative length of data is %s seconds \n', mfilename, runnum, num2str(runLengthInSeconds), num2str(samplesToAdd/hdr.Fs));          
+            end
+        end
+        % Check channel statuses
+        if ~exist('previousChannels', 'var') 
+            previousChannels = channels; 
+        else
+            if isfield(summary(channels), 'status')
+                if ~isequal(channels.status, previousChannels.status)  
+                     % This means one or more electrodes are bad in one session
+                     % but not another. For now, label the electrode as good
+                     % across all sessions:
+                     channels.status(~strcmp(channels.status, previousChannels.status)) = {'good'};
+                     % and assume bad channels/epochs will removed at later
+                     % stages.
+                     % TO DO: find some smarter way to deal with this problem
+                end
             end
         end
     end
 end
+
 data = allData;
 events = allEvents;
-channels.subject_name = repmat({subject},[height(channels) 1]);
+
+if exist('channels', 'var') 
+    channels.subject_name = repmat({subject},[height(channels) 1]);
+else
+    channels = [];
+end
+
 end
