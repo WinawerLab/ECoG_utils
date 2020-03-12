d36c68689b98c19a9ccd9d0a86c9404b89b211e7
irisgroen <iris.groen@nyu.edu>
2020-03-11 21:52:31 -0400

    updates to include a 110Hz only broadband

--- scripts/bairECOG_analyze_umcudrouwen.m
+++ scripts/bairECOG_analyze_umcudrouwen.m
@@ -5,65 +5,137 @@ subject = 'umcudrouwen';
 bidsEcogRereference(projectDir, subject);
 
 %% broadband 
-%bands             = [[60 70]; [70 80]; [80 90]; [110 120]; [120 130]; [130 140]; [160 170]; [170 180]; [180 190]];
+
+% bidsEcogBroadband(projectDir, subject, [sessions], [tasks], [runnums], ...
+%    [bands], [method], [inputFolder], [outputFolder], [description], [savePlot])
+
+outputFolder      = 'ECoGBroadband_include110Hz';
+bands             = [[60 70]; [70 80]; [80 90]; [110 120]; [120 130]; [130 140]; [160 170]; [170 180]; [180 190]];
+bidsEcogBroadband(projectDir, subject, [], [], [], bands, [], [], outputFolder);
+
+outputFolder      = 'ECoGBroadband_exclude110Hz';
 bands             = [[60 70]; [70 80]; [80 90]; [120 130]; [130 140]; [160 170]; [170 180]; [180 190]];
-bidsEcogBroadband(projectDir, subject, [], [], [], bands);
+bidsEcogBroadband(projectDir, subject, [], [], [], bands, [], [], outputFolder);
 
-%% plot
-clear specs;
+outputFolder      = 'ECoGBroadband_110Hz';
+bands             = [105 115];
+method            = @(bp) abs(hilbert(bp)).^2; % geomean should be eliminated when filtering in single band
+bidsEcogBroadband(projectDir, subject, [], [], [], bands, method, [], outputFolder);
 
-session           = 'umcuiemu01';
-task              = 'vtstemporalpattern';
-%inputFolder       = 'ECoGBroadband';
-%description       = 'broadband';
-
-inputFolder       = 'ECoGCAR';
-description       = 'reref';
-
-%specs.stim_names  = {'thumb', 'index', 'middle', 'ring', 'little'};
-specs.epoch_t     = [-0.5 0.5];
-specs.base_t      = [-0.5 -0.1];
-specs.plot_type   = 'singletrial';
-specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
-%specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
-
-%specs.chan_names  = {'C03', 'C04', 'C11', 'C12'};
-specs.chan_names  = {'C14', 'C15', 'C17', 'C18'};
-
-% specs.chan_names  = {'C'};
-% specs.subplotdims = [4 8];
-% specs.subplotidx  = [32:-1:1];
-
-%specs.plot_ylim   = [-1 6];
-
+%% plot broadband timecourses for temporal conditions
 savePlot = 1;
 
-bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot);
-
-
-%%
+session           = 'umcuiemu01';
+task              = 'vtstemporalpattern';
+inputFolder       = 'ECoGBroadband_exclude110Hz';
+description       = 'broadband';
 
 clear specs;
 
-session           = 'umcuiemu01';
-task              = 'vtstemporalpattern';
+specs.epoch_t     = [-0.4 1.8];
+specs.base_t      = [-0.4 -0.1];
+specs.plot_ylim   = [-2 10];
 
-%specs.epoch_t     = [-0.5 0.5];
-%specs.plot_type   = 'singletrial';
-specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
-%specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
-
-%specs.chan_names  = {'C03', 'C04', 'C11', 'C12'};
-%specs.chan_names  = {'C14', 'C15', 'C17', 'C18'};
-
+% all channels on the grid
 specs.chan_names  = {'C'};
 specs.subplotdims = [4 8];
 specs.subplotidx  = [32:-1:1];
 
-%specs.plot_ylim   = [-1 6];
+specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); close
 
+specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); close
+
+% subsets of channels
+specs.subplotdims = [2 2];
+specs.subplotidx  = [4:-1:1];
+
+specs.chan_names  = {'C03', 'C04', 'C11', 'C12'};
+
+specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); close
+
+specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); close
+
+specs.chan_names  = {'C14', 'C15', 'C22', 'C23'};
+
+specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); close
+
+specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); close
+
+specs.chan_names  = {'C17', 'C18','C25', 'C26', };
+
+specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); close
+
+specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); close
+
+%% plot ERP time courses (to check onsets)
+
+inputFolder       = 'ECoGCAR';
+description       = 'reref';
+
+clear specs;
+
+specs.plot_type   = 'singletrial';
+specs.epoch_t     = [-0.1 0.1];
+specs.base_t      = [-0.5 -0.1];
+
+specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
+specs.chan_names  = {'C03', 'C04', 'C11', 'C12'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); %close;
+
+specs.chan_names  = {'C14', 'C15', 'C17', 'C18'};
+bidsEcogPlotTrials(projectDir, subject, session, task, [], inputFolder, description, specs, savePlot); %close;
+
+%% spectra
 savePlot = 1;
+clear specs;
 
-bidsEcogPlotSpectra(projectDir, subject, session, task, [], [], [], specs, savePlot);
+specs.epoch_t     = [-1 2];
+specs.fft_blank_t = [-0.8 -0.1];
+specs.fft_w       = 0.4;
+specs.fft_ov      = 0.1;
+specs.plot_ylim   = [10^-3 10^3];
 
+% all channels on the grid
+specs.chan_names  = {'C'};
+specs.subplotdims = [4 8];
+specs.subplotidx  = [32:-1:1];
 
+specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
+specs.fft_stim_t  = [0 1.2];
+bidsEcogPlotSpectra(projectDir, subject, session, task, [], [], [], specs, savePlot); close;
+
+specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
+specs.fft_stim_t  = [0 1.8];
+bidsEcogPlotSpectra(projectDir, subject, session, task, [], [], [], specs, savePlot); close;
+
+% subsets of channels
+specs.subplotdims = [2 2];
+specs.subplotidx  = [4:-1:1];
+
+specs.chan_names  = {'C03', 'C04', 'C11', 'C12'};
+
+specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
+specs.fft_stim_t  = [0 1.2];
+bidsEcogPlotSpectra(projectDir, subject, session, task, [], [], [], specs, savePlot); close;
+
+specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
+specs.fft_stim_t  = [0 1.8];
+bidsEcogPlotSpectra(projectDir, subject, session, task, [], [], [], specs, savePlot); close;
+
+specs.chan_names  = {'C14', 'C15', 'C17', 'C18'};
+
+specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
+specs.fft_stim_t  = [0 1.2];
+bidsEcogPlotSpectra(projectDir, subject, session, task, [], [], [], specs, savePlot); close;
+
+specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
+specs.fft_stim_t  = [0 1.8];
+bidsEcogPlotSpectra(projectDir, subject, session, task, [], [], [], specs, savePlot); close;
