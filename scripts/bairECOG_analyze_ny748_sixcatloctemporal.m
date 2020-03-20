a1021815eb1f62654dbcf30c918c98c832da4f3a
Iris Groen <iris.groen@nyu.edu>
2020-03-19 21:31:48 -0400

    updates from laptop

--- scripts/bairECOG_analyze_ny748_sixcatloctemporal.m
+++ scripts/bairECOG_analyze_ny748_sixcatloctemporal.m
@@ -1,4 +1,34 @@
+
+clear specs;
+
+projectDir        = '/Volumes/server/Projects/BAIR/Data/BIDS/visual'; 
+subject           = 'som748';
+session           = 'nyuecog04';
+task              = 'sixcatloctemporal';
+%session           = 'nyuecog01';
+%task              = 'temporalpattern';
+runnums           = '01';
+%specs.stim_names  = {'BODIES', 'BUILDINGS', 'FACES', 'OBJECTS', 'SCENES', 'SCRAMBLED'};
+
+specs.stim_names  = {'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
+%specs.stim_names  = {'TWOPULSE-1-0', 'TWOPULSE-2-0', 'TWOPULSE-3-0', 'TWOPULSE-4-0', 'TWOPULSE-5-0', 'TWOPULSE-6-0'};
+%specs.stim_names  = {'TWOPULSE-1-1', 'TWOPULSE-2-1', 'TWOPULSE-3-1', 'TWOPULSE-4-1', 'TWOPULSE-5-1', 'TWOPULSE-6-1'};
+%specs.stim_names  = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6'};
+
+%specs.stim_names  = {'TWOPULSE-1-FACES', 'TWOPULSE-2-FACES', 'TWOPULSE-3-FACES', 'TWOPULSE-4-FACES', 'TWOPULSE-5-FACES', 'TWOPULSE-6-FACES'};
+%specs.stim_names  = {'ONEPULSE-1-FACES', 'ONEPULSE-2-FACES', 'ONEPULSE-3-FACES', 'ONEPULSE-4-FACES', 'ONEPULSE-5-FACES', 'ONEPULSE-6-FACES'};
+specs.chan_names  = {'GB018', 'GB034'};
+specs.plot_ylim   = [-1 6];
+specs.plot_cmap   = 'copper';
+specs.plot_includelegend = 0;
+
+bidsEcogPlotTrials(projectDir, subject, session, task, runnums, [], [], specs, 1);
+%bidsEcogPlotTrials(projectDir, subject, session, task);
+
+%%
+
 tbUse temporalECoG;
+
 
 %% get data
 projectDir        = '/Volumes/server/Projects/BAIR/Data/BIDS/visual'; 
@@ -15,6 +45,7 @@ chan_names        = {'GB'};   % which channels to include
 stim_names        = {'ONEPULSE-1', 'ONEPULSE-2', 'ONEPULSE-3', 'ONEPULSE-4', 'ONEPULSE-5', 'ONEPULSE-6', ...
                      'TWOPULSE-1', 'TWOPULSE-2', 'TWOPULSE-3', 'TWOPULSE-4', 'TWOPULSE-5', 'TWOPULSE-6'};
                     % how to group the stimuli
+stim_names        = {'BODIES', 'BUILDINGS', 'FACES', 'OBJECTS', 'SCENES', 'SCRAMBLED'};
 
 % load data
 dataPath = fullfile(projectDir, 'derivatives', inputFolder);
